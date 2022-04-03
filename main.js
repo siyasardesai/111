2060de7a81bb6b4f5805c5ddf6ce2d33adcb9365
siyasardesai <71547814+siyasardesai@users.noreply.github.com>
2022-04-03 16:30:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-prediction_1 = ""
-prediction_2 = ""
 
 
-      
-function take_snapshot()
-{
-    Webcam.snap(function(data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
-    });
-}
-
-
-function speak(){
-  var synth = window.speechSynthesis;
-  speak_data_1 = "The first prediction is " + prediction_1;
-  speak_data_2 = "And the second prediction is " + prediction_2;
-  var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
-  synth.speak(utterThis);
-}
 
